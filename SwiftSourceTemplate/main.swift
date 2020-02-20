8df4f1f193327988104c80faa02dc1c2367ec841
Hugo Gonzalez <hugogonzalez@granular.ag>
2020-02-20 12:24:20 -0800

    Initial Commit

---
+++ SwiftSourceTemplate/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftSourceTemplate
+//
+//  Created by Hugo Gonzalez on 2/20/20.
+//  Copyright © 2020 Hugo Gonzalez. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
